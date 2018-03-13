24646fe6661d857f2e1098df80614bafce87c4a4
tim <tim@192.168.0.110>
2018-03-13 16:27:23 +0800

    init

---
+++ Classes/UIView+WXXLayout.h
@@ -0,0 +1,25 @@
+//
+//  UIView+WXXLayout.h
+//  Demo
+//
+//  Created by tim on 2018/3/13.
+//  Copyright © 2018年 WuXianXiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (WXXLayout)
+
+@property (nonatomic, assign) CGFloat x;
+
+@property (nonatomic, assign) CGFloat y;
+
+@property (nonatomic, assign) CGFloat width;
+
+@property (nonatomic, assign) CGFloat height;
+
+@property (nonatomic, assign) CGFloat centerX;
+
+@property (nonatomic, assign) CGFloat centerY;
+
+@end
